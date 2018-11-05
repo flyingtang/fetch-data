62dcb9106881ba20a17001fd0e9ac4c27bfe62c0
xiaogang <715709084@qq.com>
2018-11-06 00:15:40 +0800

    update config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql")
 
 var connection = mysql.createConnection({
-    host: 'vinda-mysql',
+    host: 'ixiaotang.cn',
     user: 'root',
     password: 'tt1234',
-    port: 3306,
-    database: 'vinda_pro'
+    port: 3307,
+    database: 'vinda_dev'
 });
 connection.connect();
 
